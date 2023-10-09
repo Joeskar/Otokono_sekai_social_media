b49a11275472aadbad59efbdefd43fb6862c6b35
Askar Social <ernur.social@bk.ru>
2023-10-09 07:52:34 +0600

    added jwt authentication

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
 if settings.DEBUG:
