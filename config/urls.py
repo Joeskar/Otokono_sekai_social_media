a0513b17132e6549aefc1575510332e0e55a1179
Askar Social <ernur.social@bk.ru>
2023-10-10 06:59:16 +0600

    added routers to urls

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include('apps.routers'))
 ]
 
 if settings.DEBUG:
