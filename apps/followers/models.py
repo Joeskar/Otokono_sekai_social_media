73be7b2a9da669510529eec08f510a150b9c79f7
Askar Social <ernur.social@bk.ru>
2023-10-11 07:44:24 +0600

    added new model named 'Follower'

--- apps/followers/models.py
+++ apps/followers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Follower(models.Model):
+    """Follower model"""
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followers')
+
+    class Meta:
+        unique_together = ('follower', 'following')
